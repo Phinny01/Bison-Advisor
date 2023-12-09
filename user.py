ef27d7a617b4394e4ce76ccd55679dae90fc0af0
SasheO <76496417+SasheO@users.noreply.github.com>
2023-12-08 19:41:11 -0500

    Update user.py

--- user.py
+++ user.py
@@ -70,7 +70,7 @@ class Student(User):
         self.advisor = advisor_id
 
     def request_advice(self, message):
-        # Implementation for requesting advice from the advisor
+        # TODO: Implementation for requesting advice from the advisor
         print(f"Student {self.username} sent a message to their advisor: {message}")
     
     def save_values(self):
@@ -114,68 +114,13 @@ class SystemAdmin(User):
 
     @staticmethod
     def create_student_objects(student_data):
-        students_dict = {}
-
-        try:
-            with open('students.pkl', 'rb') as file:
-                students_dict = pickle.load(file)
-
-        except FileNotFoundError:
-            pass
-
-        for data in student_data:
-            student = Student(data['username'], data['email'], data['password'], data['department'])
-            students_dict[student.email] = student
-            # SystemAdmin.assign_student_to_advisor(student)
-
-        with open('students.pkl', 'wb') as file:
-            pickle.dump(students_dict, file)
+        pass
 
     @staticmethod
     def create_advisor_object(advisor_data):
-        advisors_dict = {}
-
-        try:
-            with open('advisors.pkl', 'rb') as file:
-                advisors_dict = pickle.load(file)
-        except FileNotFoundError:
-            pass
-
-        for data in advisor_data:
-            advisor = AcademicAdvisor(data['username'], data['email'], data['password'], data['department'])
-            advisors_dict[advisor.email] = advisor
-
-        with open('advisors.pkl', 'wb') as file:
-            pickle.dump(advisors_dict, file)
-
-        return advisor
+        pass
 
 
     @staticmethod
     def assign_student_to_advisor(student):
-        advisors_dict = {}
-
-
-        with open('advisors.pkl', 'rb') as file:
-            advisors_dict = pickle.load(file)
-
-
-        # Filter advisors by department
-        advisors_in_department = [advisor for advisor in advisors_dict.values() if advisor.department == student.department]
-        for advisors in advisors_in_department:
-            print(advisors.email, advisors.department)
-        print(student.department)
-
-        # Find the advisor with the least number of students
-        selected_advisor = min(advisors_in_department, key=lambda advisor: len(advisor.advisee_student_ids))
-
-
-        # Link the student to the selected advisor (put student ids in list of student associated to advisor)
-        selected_advisor.link_student(student.user_id)
-
-        # Update the advisors dictionary
-        advisors_dict[selected_advisor.email] = selected_advisor
-
-        # Save the updated advisors dictionary to the file
-        with open('advisors.pkl', 'wb') as file:
-            pickle.dump(advisors_dict, file)
+        pass
