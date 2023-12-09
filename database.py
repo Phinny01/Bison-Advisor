24e32e110fc8ae578cca973da99648892d890e00
Joseph Sankah <josephsankah0@gmail.com>
2023-12-08 22:04:15 -0500

    got a code snippet to dump static info for checklist

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from datetime import datetime, timezone
+
 
 # Use a service account.
 cred = credentials.Certificate('service_account.json')
@@ -8,3 +10,17 @@ cred = credentials.Certificate('service_account.json')
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+#city_ref = db.collection("bison-advisor").document("checklist")
+
+# city_ref.set({"capital": True}, merge=True)
+
+data = {
+    "course_subject": "CSCI 100",
+    "course_title": "Intro to Computer Science",
+    "credit": 3,
+    "grade": "A",
+    "year": "Freshman"
+}
+
+db.collection("bison-advisor").document("checklist").set(data)
