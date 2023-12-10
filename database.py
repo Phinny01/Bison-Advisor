dd3ecf03f720ef3cad56264e8d49c2aaf7cf6e45
Joseph Sankah <josephsankah0@gmail.com>
2023-12-09 19:38:49 -0500

    refactored

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from datetime import datetime, timezone
 
 
 # Use a service account.
@@ -11,9 +10,6 @@ app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-#city_ref = db.collection("bison-advisor").document("checklist")
-
-# city_ref.set({"capital": True}, merge=True)
 
 data = {
     "course_subject": "CSCI 100",
